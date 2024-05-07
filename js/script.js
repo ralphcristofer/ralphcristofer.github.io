d9096917e93b68fc8fa1d0f80cbe9a1c73ae58c9
ralphcristofer <rcpfugaban@gmail.com>
2024-05-07 11:27:53 -0400

    Adding nav menus and assets

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".mobile-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
